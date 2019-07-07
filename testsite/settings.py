c5a585c4b2954b7d4b2a7ed6b570820dcf16f940
Vlasov Sergei <vlasov@vlasov-macbookair.local>
2019-07-07 11:51:53 +0300

    add allowed host

--- testsite/settings.py
+++ testsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xu)nx%ib4itmxylr!fx2(&r0c5lbcb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cryptic-bayou-59690.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
